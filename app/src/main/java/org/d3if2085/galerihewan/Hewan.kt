039d81c6388d0711f62656545533717d9b329e1b
kania_puspa_harnum <kaniapuspahr@gmail.com>
2021-03-12 19:07:31 +0700

    Menambahkan gambar pada list task 3.5

--- app/src/main/java/org/d3if2085/galerihewan/Hewan.kt
+++ app/src/main/java/org/d3if2085/galerihewan/Hewan.kt
@@ -1,5 +1,7 @@
 package org.d3if2085.galerihewan
 
     data class Hewan(
-            val nama: String,     val namaLatin: String
+            val nama: String,
+            val namaLatin: String,
+            val imageResId: Int
     )
