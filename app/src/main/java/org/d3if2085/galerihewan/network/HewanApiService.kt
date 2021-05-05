64ea593b132c3ed2f2fd13cb9b8fd2ab804c5c5f
kania_puspa_harnum <kaniapuspahr@gmail.com>
2021-05-06 01:33:53 +0700

    Menampilkan gambar dari internet Task 3.5

--- app/src/main/java/org/d3if2085/galerihewan/network/HewanApiService.kt
+++ app/src/main/java/org/d3if2085/galerihewan/network/HewanApiService.kt
@@ -8,21 +8,24 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 
-private const val BASE_URL = "https://dif.indraazimi.com/"
-private val moshi = Moshi.Builder()
+    private const val BASE_URL = "https://dif.indraazimi.com/"
+    private val moshi = Moshi.Builder()
         .add(KotlinJsonAdapterFactory())
         .build()
-private val retrofit = Retrofit.Builder()
+    private val retrofit = Retrofit.Builder()
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .baseUrl(BASE_URL)
         .build()
 
-interface HewanApiService {
+    interface HewanApiService {
     @GET("listhewan.json")
     suspend fun getHewan(): List<Hewan>
 }
-object HewanApi {
+    object HewanApi {
     val service: HewanApiService by lazy {
         retrofit.create(HewanApiService::class.java)
+    }
+    fun getHewanUrl(nama: String): String {
+        return BASE_URL + "hewan/$nama.jpg"
     }
 }
--- app/src/main/java/org/d3if2085/galerihewan/ui/main/MainAdapter.kt
+++ app/src/main/java/org/d3if2085/galerihewan/ui/main/MainAdapter.kt
@@ -3,10 +3,13 @@ package org.d3if2085.galerihewan.ui.main
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import org.d3if2085.galerihewan.R
 import org.d3if2085.galerihewan.model.Hewan
 import org.d3if2085.galerihewan.databinding.ListItemBinding
+import org.d3if2085.galerihewan.network.HewanApi
 
-    class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
         private val data = mutableListOf<Hewan>()
         fun updateData(newData: List<Hewan>) {
             data.clear()
@@ -16,11 +19,17 @@ import org.d3if2085.galerihewan.databinding.ListItemBinding
     class ViewHolder(private val binding: ListItemBinding) :
             RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(hewan: Hewan) = with(binding) {
-            namaTextView.text = hewan.nama
-            latinTextView.text = hewan.namaLatin
-            imageView.setImageResource(hewan.imageResId)
+        fun bind(hewan: Hewan) {
+            with(binding) {
+                namaTextView.text = hewan.nama
+                latinTextView.text = hewan.namaLatin
+                Glide.with(imageView.context)
+                    .load(HewanApi.getHewanUrl(hewan.imageId))
+                    .error(R.drawable.ic_baseline_broken_image_24)
+                    .into(imageView)
+            }
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
